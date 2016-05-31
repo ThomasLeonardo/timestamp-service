17dc83c2ee469b257b1a7b5f97deee66e2871cab
Thomas Leonardo <tl.rp@hotmail.com>
2016-05-31 17:59:21 +0000

    test

--- server.js
+++ server.js
@@ -5,28 +5,29 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
-
+var moment= require('moment');
 var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
+app.get('/:date',function(req,res){
+	var formats=['x','MMMM DD, YYYY'];
+	var date=moment(req.params.date, formats, true);
+	var dates={};
+	if(date.isValid())
+	{
+		dates['unix']=date.format('x');
+		dates['natural']=date.format('MMMM DD, YYYY');
+	}
+	else
+	{
+		dates['unix']=null;
+		dates['natural']=null;
+	}
+	res.end(JSON.stringify(dates));
+	
+});
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
